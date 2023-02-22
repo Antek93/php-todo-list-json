d53d9063ec02d1ce1e8b324ea13a828ebcd8b27f
Antek93 <antonio.savino1993@gmail.com>
2023-02-22 16:47:21 +0100

    importati vue axios js css api.php e .json

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('java is working fine');
+
+const { createApp } = Vue;
+
+createApp({
+    data () {
+        return {
+            message: 'I am working'
+        };
+
+    },
+    methods: {
+
+    },
+    created() {
+        console.log(axios)
+    }
+
+}).mount('#app')
